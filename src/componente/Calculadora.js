f2fb5f93ef46ffeb3dbff4119fd77ba0d9330242
Eliel Maia <elieljuniorm@gmail.com>
2022-05-21 16:29:54 -0300

    criando funcao de calculo

--- src/componente/Calculadora.js
+++ src/componente/Calculadora.js
@@ -3,6 +3,59 @@ import React from 'react'
 import styles from './Calculadora.module.css'
 
 function Calculadora() {
+
+    function calcular(){ 
+        let tab = Number(document.getElementById("fretepeso").value)
+        let val = Number(document.getElementById("valornf").value)
+        let pes = Number(document.getElementById("pesobruto").value)
+        let cub = Number(document.getElementById("cubagem").value)
+        let fat = Number(document.getElementById("fator").value)
+        let volume = Number(document.getElementById("volume").value)
+        let alt = Number(document.getElementById("altura").value)
+        let com = Number(document.getElementById("comprimento").value)
+        let lag = Number(document.getElementById("largura").value)
+        let res = document.getElementById('resultado')
+        let dim = (alt*com*lag*volume)*fat
+        let cubft = cub*fat
+        const seguro = 0.007
+        const icms = 0.93
+        const taxa = 65.00
+    
+        if(val <= 0 || pes <= 0){
+            alert('VALOR DE NF-e OU PESO INVALIDOS, TENTE NOVAMENTE')
+        }else{
+    
+            console.log({cubft,dim,pes})
+            if(cubft > pes && cubft > dim && pes !== 0 && val !== 0){
+                let fretecb = ((val*seguro) + taxa + (cubft*tab))/icms
+                res.innerHTML = `Seu frete fica no valor de R$ ${fretecb.toFixed(2)}`
+            }
+    
+                else{
+                    console.log({cubft,dim,pes})
+                    if(cubft > pes && cubft < dim && pes !== 0 && val !== 0){
+                        let fretedim = ((val*seguro) + taxa + (dim*tab)/icms)
+                        res.innerHTML = `Seu frete fica no valor de R$ ${fretedim.toFixed(2)}`
+    
+                    }
+                    else{
+                        console.log({cubft,pes,dim})
+                        if(cubft < pes && dim < pes && pes !== 0 && val !== 0){
+                            let fretepeso = ((val*seguro) + taxa + (pes*tab)/icms)
+                            res.innerHTML = `Seu frete fica no valor de R$ ${fretepeso.toFixed(2)}`
+    
+                        }                   
+                    }
+                }
+            }
+            if(val <= 1000 && pes <=100 && pes > 50 && pes !== 0 && val !== 0){
+                res.innerHTML = `Seu frete fica na taxa mínima de R$ 125,00`
+            }else{
+                if(val <= 1000 && pes <= 50 && pes !== 0 && val !== 0){
+                res.innerHTML = `Seu frete fica na taxa mínima de R$ 95,00`}
+            }
+    }
+
     return (
 
         <div className={styles.pagina}>
@@ -11,56 +64,56 @@ function Calculadora() {
 
                 <h1>Informe os dados da Mercadoria</h1>
 
-                <form Name={styles.formulario}>
+                <form className={styles.formulario}>
 
-                    <label for="tabela">Tabela:</label>
+                    <label >Tabela:</label>
 
-                    <input type="number" name="fretepeso" id="fretepeso" placeholder="Frete peso" required className={styles.entradaDados} />
+                    <input type="number"  id="fretepeso" placeholder="Frete peso" required className={styles.entradaDados} />
 
-                    <label for="valornf">Valor de NF-e:</label>
+                    <label >Valor de NF-e:</label>
 
-                    <input type="number" name="valornf" id="valornf" placeholder="Valor NF-e" required className={styles.entradaDados} />
+                    <input type="number"  id="valornf" placeholder="Valor NF-e" required className={styles.entradaDados} />
 
-                    <label for="quantidade">Quantidade:</label>
+                    <label>Quantidade:</label>
 
-                    <input type="number" name="quantidade" id="quantidade" placeholder="Quantidade" className={styles.entradaDados} />
+                    <input type="number"  id="quantidade" placeholder="Quantidade" className={styles.entradaDados} />
 
-                    <label for="peso">Peso:</label>
+                    <label >Peso:</label>
 
-                    <input type="number" name="peso" id="pesobruto" placeholder="Peso bruto" required className={styles.entradaDados} />
+                    <input type="number"  id="pesobruto" placeholder="Peso bruto" required className={styles.entradaDados} />
 
-                    <label for="cubagem">Cubagem:</label>
+                    <label>Cubagem:</label>
 
-                    <input type="number" name="cubagem" id="cubagem" placeholder="Metro cúbico" className={styles.entradaDados} />
+                    <input type="number"  id="cubagem" placeholder="Metro cúbico" className={styles.entradaDados} />
 
-                    <label for="fator-de-cubagem">Fator de Cubagem:</label>
+                    <label>Fator de Cubagem:</label>
 
-                    <input type="number" name="fator" id="fator" placeholder="000 m³" className={styles.entradaDados} />
+                    <input type="number"  id="fator" placeholder="000 m³" className={styles.entradaDados} />
 
 
-                    <table className={styles.tabela}>
+                    <div className={styles.tabela}>
 
                         <span>
-                            <label for="altura" className={styles.tituloMedidas}>Altura</label>
-                            <input type="number" name="altura" id="altura" placeholder="0.00 m" className={styles.medidas} />
+                            <label className={styles.tituloMedidas}>Altura</label>
+                            <input type="number"  id="altura" placeholder="0.00 m" className={styles.medidas} />
                         </span>
 
                         <span>
-                            <label for="altura" className={styles.tituloMedidas}>Largura</label>
-                            <input type="number" name="comprimento" id="comprimento" placeholder="0.00 m" className={styles.medidas} />
+                            <label className={styles.tituloMedidas}>Largura</label>
+                            <input type="number"  id="comprimento" placeholder="0.00 m" className={styles.medidas} />
                         </span>
 
                         <span>
-                            <label for="altura" className={styles.tituloMedidas}>Comprimento</label>
-                            <input type="number" name="largura" id="largura" placeholder="0.00 m" className={styles.medidas} />
+                            <label className={styles.tituloMedidas}>Comprimento</label>
+                            <input type="number"  id="largura" placeholder="0.00 m" className={styles.medidas} />
                         </span>
 
                         <span>
-                            <label for="altura" className={styles.tituloMedidas}>Volume</label>
-                            <input type="number" name="volume" id="volume" placeholder="Nº Vol." className={styles.medidas} />
+                            <label className={styles.tituloMedidas}>Volume</label>
+                            <input type="number"  id="volume" placeholder="Nº Vol." className={styles.medidas} />
                         </span>
 
-                    </table>
+                    </div>
 
                     {/* <div className={styles.botoes}>
                         <input type="button" value="Adicionar" onclick="adicionar()" className={styles.botaoDimensao} />
@@ -75,7 +128,7 @@ function Calculadora() {
                 </div>
 
                 <div className={styles.botoes}>
-                    <input type="button" value="Calcular" onclick="calcular()" className={styles.botaoCalcular} />
+                    <button type='button' title='Calcular' className={styles.botaoCalcular} onClick={calcular}>COTAR</button>
                 </div>
             </main>
         </div>
