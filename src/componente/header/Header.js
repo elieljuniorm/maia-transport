82f1845c298c86d498faf534c87e0c110b5d7f6d
Eliel Maia <elieljuniorm@gmail.com>
2022-07-11 16:33:10 -0300

    troca de propriedade de site

--- src/componente/header/Header.js
+++ src/componente/header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Jurua from '../img/jurua.png'
+import MaiaTransport from '../img/android-chrome-512x512.png'
 import styles from './Header.module.css'
 
 function Header() {
     return (
         <>
             <div className={styles.fundoCapa}>
-                <img src={Jurua} alt='Jurua Logística e Transportes LTDA' className={styles.perfilFoto} />
-                <p>Jurua Logística e Transportes</p>
+                <img src={MaiaTransport} alt='Maia Transport' className={styles.perfilFoto} />
+                <p>Maia Transport - Frete Rápido</p>
             </div>
         </>
     )
